69a5e2a0cf26a93e52ee505a0f2359778eac35cd
frye-t <tyler.frye42@gmail.com>
2023-11-29 08:56:11 -0500

    feat: host/join rooms + start game basic func

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { socket } from './socket';
 import { ConnectionState } from './components/ConnectionState';
 import { ConnectionManager } from './components/ConnectionManager';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const [inputValue, setInputValue] = useState(''); // State variable for input value
 
   useEffect(() => {
     const onConnect = () => {
@@ -19,19 +21,83 @@ function App() {
       setIsConnected(false);
     };
 
+    const onMessageReceived = () => {
+      console.log('message received');
+    };
+
+    const onPlayerJoined = () => {
+      console.log('A new player joined:');
+    };
+
+    const onGameStarted = () => {
+      console.log('Game has started!');
+    };
+
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
+    socket.on('messageReceived', onMessageReceived);
+    socket.on('playerJoined', onPlayerJoined);
+    socket.on('gameStarted', onGameStarted);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
+      socket.off('messageReceived', onMessageReceived);
+      socket.off('playerJoined', onPlayerJoined);
+      socket.off('gameStarted', onGameStarted);
     };
-  });
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setInputValue(value.replace(/[^0-9]/g, ''));
+  };
+
+  const handleButtonClick = () => {
+    console.log('Button clicked with value:', inputValue);
+    socket.emit('joinRoom', { roomCode: inputValue });
+  };
+
+  const hostGame = async () => {
+    console.log('Awaiting hostRoom');
+    const { data } = await axios.get('http://localhost:3000/getRoomCode');
+    const { roomCode } = data;
+    console.log('Trying to join room with code:', roomCode);
+    socket.emit('hostRoom', { roomCode });
+  };
+
+  const sendMessage = () => {
+    console.log('sending message');
+    socket.emit('message');
+  };
+
+  const startGame = () => {
+    console.log('Starting game clicked');
+    socket.emit('startGame');
+  };
 
   return (
     <div className="App">
       <ConnectionState isConnected={isConnected} />
       <ConnectionManager />
+      <button onClick={hostGame}>Host</button>
+      <form>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter numbers"
+        />
+        <button type="button" onClick={handleButtonClick}>
+          Join
+        </button>
+        <button type="button" onClick={sendMessage}>
+          Send Message
+        </button>
+        <button type="button" onClick={startGame}>
+          Start Game
+        </button>
+      </form>
     </div>
   );
 }
--- src/socket.ts
+++ src/socket.ts
@@ -2,14 +2,30 @@ import { io, Socket } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+
+interface RoomData {
+  roomCode: string;
+  options? :ServerOptions;
+}
+
+interface ServerOptions {
+  option1: string;
+}
+
 interface ServerToClientEvents {
   noArg: () => void;
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
+  messageReceived: () => void;
+  playerJoined: () => void;
+  gameStarted: () => void;
 }
 
 interface ClientToServerEvents {
-  hello: () => void;
+  hostRoom: (data: RoomData) => void;
+  joinRoom: (data: RoomData) => void;
+  message: () => void;
+  startGame: () => void;
 }
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
